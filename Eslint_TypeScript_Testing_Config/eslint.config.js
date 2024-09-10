0a9eea09930840e87bc2dd8712f5c49dd415b32f
Stoica Robert <stoicarobert38@yahoo.com>
2024-09-10 19:58:34 +0300

    Problem  - Version Problem of Eslint

---
+++ Eslint_TypeScript_Testing_Config/eslint.config.js
@@ -0,0 +1,73 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import react from 'eslint-plugin-react';
+import vitest from 'eslint-plugin-vitest';
+import jestDom from 'eslint-plugin-jest-dom';
+import testingLibrary from 'eslint-plugin-testing-library';
+
+export default tseslint.config(
+  { ignores: ['dist', 'build'] },
+
+  {
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommendedTypeChecked,
+      ...tseslint.configs.stylisticTypeChecked,
+      vitest.configs.recommended,
+      jestDom.configs['flat/recommended'],
+      testingLibrary['flat/recommended'],
+    ],
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+      globals: { ...vitest.environments.env.globals, ...globals.browser },
+      parserOptions: {
+        project: ['./tsconfig.node.json', './tsconfig.app.json'],
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    }, // What version of react we use to be available to the eslint-plugin-react
+
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+      react,
+      'plugin:testing-library/react': testingLibrary,
+      'plugin:vitest': vitest,
+      'plugin:jest-dom/recommended': jestDom,
+    },
+    rules: {
+      //-----------------------------------------
+      //React Hooks Rules
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+      //-----------------------------------------
+
+      //-----------------------------------------
+      // React Plugin Rules
+      ...react.configs.recommended.rules,
+      ...react.configs['jsx-runtime'].rules,
+      'react/react-in-jsx-scope': 'off',
+      'react/jsx-no-target-blank': 'off',
+      //-----------------------------------------
+
+      //-----------------------------------------
+      //TypeScript Rules
+      '@typescript-eslint/no-misused-promises': 'off',
+      '@typescript-eslint/consistent-type-definitions': 'off',
+      '@typescript-eslint/no-unused-vars': 'off',
+      //-----------------------------------------
+    },
+  }
+);
--- Eslint_TypeScript_Testing_Config/vite.config.ts
+++ Eslint_TypeScript_Testing_Config/vite.config.ts
@@ -2,13 +2,22 @@ import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 import eslint from 'vite-plugin-eslint';
 const typedEslintPlugin = eslint as () => import('vite').Plugin;
-//https:vitejs.dev/config/
+
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), typedEslintPlugin()],
+  plugins: [
+    react(),
+    eslint({
+      include: ['src/**/*.ts', 'src/**/*.tsx', 'src/**/*.js', 'src/**/*.jsx'],
+      cache: false, // optional, disables ESLint caching to avoid old results
+    }),
+  ],
   test: {
     globals: true,
     environment: 'jsdom',
-    // this points to the setup file that we created earlier     setupFiles: './src/setupTests.ts',     // you might want to disable the css: true line, since we don't have
+    // this points to the setup file that we created earlier
+    setupFiles: './src/setupTests.ts',
+    // you might want to disable the `css: true` line, since we don't have
     // tests that rely on CSS -- and parsing CSS is slow.
     // I'm leaving it in here becasue often people want to parse CSS in tests.
     css: true,
